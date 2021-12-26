03176b39843031faf43e109e95c0f92d781e6f81
yugaopeng <18516272648@163.com>
2021-12-26 21:04:36 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,8 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('signup', 'UsersController@create ')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
 
+Route::resource('users', 'UsersController');
 
-
-
-
-
-Route:
 

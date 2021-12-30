55200ac1420468631bc06aa47a463f02eebac887
yugaopeng <18516272648@163.com>
2021-12-30 16:07:09 +0800

    更改用户资料

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -5,7 +5,7 @@
       @if (Auth::check())
         <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">用户列表</a></li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             {{ Auth::user()->name }}
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">

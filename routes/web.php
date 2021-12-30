1c6665035c8b88b47941f4f388ca01af357033d9
yugaopeng <18516272648@163.com>
2021-12-30 21:18:27 +0800

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('p
 Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
